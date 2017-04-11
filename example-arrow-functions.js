1a42d12e13e8ec85393bca12d2870ff81c682614
VictorGuzman <viaguz@gmail.com>
2017-04-10 21:40:27 -0600

    Add code: ES6 arrow function refactor.

---
+++ example-arrow-functions.js
@@ -0,0 +1,26 @@
+// var names = ['Victor', 'Andrew', 'Jen', 'Julie'];
+//
+// names.forEach(function (name) {
+//   console.log('forEach', name);
+// });
+//
+// names.forEach((name) => {
+//   console.log('arrowFunc', name);
+// });
+//
+// names.forEach((name) => console.log(name));
+
+// var returnMe = (name) => name + '!';
+// console.log(returnMe('Victicorsh'));
+
+// var person = {
+//   name: 'Vic',
+//   greet: function () {
+//     names.forEach((name) => {
+//       console.log(this.name + ' says hi to ' + name);
+//     });
+//   }
+// };
+//
+//
+// person.greet();
