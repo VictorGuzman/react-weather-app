e1e850400f7a6d1c6532ee263dc87c267c7ff5f7
VictorGuzman <viaguz@gmail.com>
2017-04-17 13:44:37 -0600

    Add styles to About page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,25 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">
+            React
+          </a>
+          - This was the JavaScript framework I used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">
+            Open Weather Map
+          </a>
+          - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
